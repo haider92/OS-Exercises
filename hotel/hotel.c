d1b135ff09badb4447db99b35fb36b543c107925
Ian Duffy <ian@ianduffy.ie>
2014-03-19 09:18:31 +0000

    Update hotel.c

--- hotel/hotel.c
+++ hotel/hotel.c
@@ -1,15 +1,6 @@
 /**
 * CA321 - OS Design and Implementation
 * Assignment 2 - Hotel Wakeup call system
-*
-* @author Ian Duffy, 11356066
-* @author Darren Brogan, 11424362
-* @author Peter Morgan, 11314976
-*
-* This project is our own work. We have not recieved assistance beyond what is
-* normal, and we have cited any sources from which we have borrowed. We have
-* not given a copy of our work, or a part of our work, to anyone. We are aware
-* that copying or giving a copy may have serious consequences.
 **/
 
 #include <stdio.h>
